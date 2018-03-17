565572c25ab0eec879f4b74713186c89861e88c3
MSandeman <37485711+MSandeman@users.noreply.github.com>
2018-03-17 20:19:36 +0000

    Add files via upload

---
+++ Assets/DialogTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PrintEvent(string s)
+    {
+        Debug.Log("PrintEvent: " + s + "called at: " + Time.time);
+    }
+}
---
+++ Assets/MusicFadeInOut.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicFadeInOut : MonoBehaviour {
+    private AudioSource[] clipSources;
+    public AudioSource mainSource;
+    public AudioSource dialogSource;
+
+
+	// Use this for initialization
+	void Start () {
+		clipSources = GetComponents<AudioSource>();
+        mainSource = clipSources[0];
+        dialogSource = clipSources[1];
+
+        mainSource.volume = (float)1.0;
+        dialogSource.volume = (float)0.0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FadeOUT()
+    {
+        if (mainSource.volume > 0)
+        {
+            mainSource.volume -= (float)0.1;
+        }
+        if(mainSource.volume < 0.5)
+        {
+            FadeIN();
+        }
+    }
+
+    public void FadeIN()
+    {
+        if(dialogSource.volume < 1)
+        {
+            dialogSource.volume += (float)0.1;
+        }
+    }
+}
